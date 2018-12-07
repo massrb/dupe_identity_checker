e5c3c528a9fc06555f39263f8aae5430a8bb5c15
lguild <massrubydev@gmail.com>
2018-12-07 11:17:30 -0500

    code cleanup

--- app/controllers/checker_controller.rb
+++ app/controllers/checker_controller.rb
@@ -1,26 +1,26 @@
 
 
 class CheckerController < ApplicationController
-  
-  def index
-  	@links = [txt: 'Upload', path: '#', class: 'upload-csv']
-  end
-
-  def upload_csv
-    csv_file = params[:csv_file]
-    tmp_file = csv_file.tempfile
-    pub_path = Rails.root.join('public', 'uploads', csv_file.original_filename)
-    File.delete(pub_path) if File.exist?(pub_path)	
-
-	File.open(pub_path, 'wb') do |file|
-	  file.write(tmp_file.read)
+	
+	def index
+		@links = [txt: 'Upload', path: '#', class: 'upload-csv']
 	end
 
-  result = IdChecker::FileReader.read(pub_path)
-  puts result.out
+	def upload_csv
+		csv_file = params[:csv_file]
+		tmp_file = csv_file.tempfile
+		pub_path = Rails.root.join('public', 'uploads', csv_file.original_filename)
+		File.delete(pub_path) if File.exist?(pub_path)	
+
+	File.open(pub_path, 'wb') do |file|
+		file.write(tmp_file.read)
+	end
+
+	result = IdChecker::FileReader.read(pub_path)
+	puts result.out
 
 	render json: result.data.to_json
 
-  end
+	end
 
 end
